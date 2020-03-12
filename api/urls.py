b3e1c55966bf42a6274ab82c2a1b9da95a4b7975
Dadfar <dadfar.momeni@gmail.com>
2020-03-13 01:11:48 +0330

    starting by documentation

--- api/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.contrib import admin
-from django.urls import path, re_path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    re_path('api/(?P<version>(v1|v2))/', include('music.urls'))
-]
